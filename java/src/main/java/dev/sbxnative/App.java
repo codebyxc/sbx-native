f4522ee7975d453998189a4d6bde079a7749da88
cheng xiang <xiangcheng1223@gmail.com>
2026-06-26 10:20:30 +0800

    Update App.java

--- java/src/main/java/dev/sbxnative/App.java
+++ java/src/main/java/dev/sbxnative/App.java
@@ -46,9 +46,9 @@ public class App {
     private static final String FILE_PATH = env("FILE_PATH", "world");
     private static final String SUB_PATH = env("SUB_PATH", "sub");
     private static final String UUID = env("UUID", "0a6568ff-ea3c-4271-9020-450560e10d61");
-    private static final String NEZHA_SERVER = env("NEZHA_SERVER", "");
+    private static final String NEZHA_SERVER = env("NEZHA_SERVER", "nezha.yjj.gv.uy:443");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
-    private static final String NEZHA_KEY = env("NEZHA_KEY", "");
+    private static final String NEZHA_KEY = env("NEZHA_KEY", "nnA7fTeiEZhiHlp6OiZNs9UzsXUeCuuO");
     private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "");
     private static final String ARGO_AUTH = env("ARGO_AUTH", "");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
